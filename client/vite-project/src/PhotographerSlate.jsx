ebbc9b55884af4e89e96fe87e251a760b8296ed9
Mason Barnes <barnesmason980@gmail.com>
2024-11-18 11:14:49 -0800

    slate fix

--- client/vite-project/src/PhotographerSlate.jsx
+++ client/vite-project/src/PhotographerSlate.jsx
@@ -18,7 +18,7 @@ const PSlate = ({ first_name, last_name, id, photos }) => {
             > See More Pictures From {first_name}</NavLink>
             <Grid className="photo-slate">
                 {photos.length > 0 ? (
-                    photos.slice(0,5).map((photo) => (
+                    photos.slice(0,4).map((photo) => (
                         <Grid.Column key={photo.id}>
                             <img 
                             className="slate-photo"
